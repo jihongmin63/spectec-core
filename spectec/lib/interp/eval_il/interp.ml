jihongmin63/spectec-core

commit 567ad861aaddd19d303f2799b684ea0d94dc4f80

refactor(cache): wrapper for cache usage, prevent HOF caching (#10)
Provides a caching convenience wrapper that takes a computation and
either retrieves the result from cache or runs the computation.
Also prevents caching high-order functions as `FuncV`s can only be
distinguished via id. This logic is ported from ETH-SpecTec.

Original-commit: https://github.com/GyeongMinDan/eth-spectec/commit/f8bad01

spectec/lib/interp/eval_il/interp.ml
@@ -901,17 +901,16 @@ and invoke_rel (ctx : Ctx.t) (id : id) (values_input : value list) :
       in
       choice attempt_rules'
     in
-    if Cache.is_cached_rule id.it then (
-      let cache_result = Cache.Cache.find !rule_cache (id.it, values_input) in
-      match cache_result with
-      | Some values_output -> Ok (ctx, values_output)
-      | None ->
-          let* ctx, values_output = attempt_rules () in
-          Cache.Cache.add !rule_cache (id.it, values_input) values_output;
-          Ok (ctx, values_output))
-    else
-      let* ctx, values_output = attempt_rules () in
-      Ok (ctx, values_output)
+    let invoke () =
+      let* _, values_output = attempt_rules () in
+      Ok values_output
+    in
+    let* values_output =
+      if Cache.is_cached_rule id.it then
+        invoke |> Cache.with_cache rule_cache (id.it, values_input)
+      else invoke ()
+    in
+    Ok (ctx, values_output)
   in
   let result = invoke_rel' () in
   Instrumentation.Hooks.notify_rel_exit ~id:id.it ~at:id.at
@@ -922,6 +921,7 @@ and invoke_rel (ctx : Ctx.t) (id : id) (values_input : value list) :
 
 and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
     (Ctx.t * value) attempt =
+  let ctx, values_input = eval_args ctx args in
   (* Clause matching *)
   let match_clause ctx_caller ctx_callee clause values_input =
     let args_input, exp_output, prems = clause.it in
@@ -933,8 +933,6 @@ and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
   in
   (* Builtin function invocation *)
   let invoke_func_builtin () =
-    (* Evaluate arguments *)
-    let ctx, values_input = eval_args ctx args in
     Instrumentation.Hooks.notify_func_enter ~id:id.it ~at:id.at
       ~values:values_input;
     (* Invoke builtin function *)
@@ -948,17 +946,7 @@ and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
       Instrumentation.Hooks.notify_clause_exit ~id:id.it ~at:id.at;
       Ok (ctx, value_output)
     in
-    if Cache.is_cached_func id.it then (
-      let cache_result = Cache.Cache.find !func_cache (id.it, values_input) in
-      match cache_result with
-      | Some value_output -> Ok (ctx, value_output)
-      | None ->
-          let* ctx, value_output = invoke_func_builtin' () in
-          Cache.Cache.add !func_cache (id.it, values_input) value_output;
-          Ok (ctx, value_output))
-    else
-      let* ctx, value_output = invoke_func_builtin' () in
-      Ok (ctx, value_output)
+    invoke_func_builtin' ()
   in
   (* User-defined function invocation *)
   let invoke_func_def () =
@@ -981,8 +969,6 @@ and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
           in
           List.map (Typ.subst_typ theta) targs
     in
-    (* Evaluate arguments *)
-    let ctx, values_input = eval_args ctx args in
     Instrumentation.Hooks.notify_func_enter ~id:id.it ~at:id.at
       ~values:values_input;
     (* Apply the first matching clause *)
@@ -1031,24 +1017,32 @@ and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
       in
       choice attempt_clauses'
     in
-    if Cache.is_cached_func id.it then (
-      let cache_result = Cache.Cache.find !func_cache (id.it, values_input) in
-      match cache_result with
-      | Some value_output -> Ok (ctx, value_output)
-      | None ->
-          let* ctx, value_output = attempt_clauses () in
-          Cache.Cache.add !func_cache (id.it, values_input) value_output;
-          Ok (ctx, value_output))
-    else
-      let* ctx, value_output = attempt_clauses () in
-      Ok (ctx, value_output)
+    let* _, value_output = attempt_clauses () in
+    Ok (ctx, value_output)
   in
   (* Main dispatch *)
-  let invoke_func' () =
-    if Builtins.is_builtin id then invoke_func_builtin ()
-    else invoke_func_def ()
+  let result =
+    let invoke_func' () =
+      let* _, value_output =
+        if Builtins.is_builtin id then invoke_func_builtin ()
+        else invoke_func_def ()
+      in
+      Ok value_output
+    in
+    let* value_output =
+      (* Skip caching for generics and HOFs *)
+      if
+        (not (Cache.is_cached_func id.it))
+        || targs <> []
+        || List.exists
+             (fun value ->
+               match value.it with Lang.Il.FuncV _ -> true | _ -> false)
+             values_input
+      then invoke_func' ()
+      else invoke_func' |> Cache.with_cache func_cache (id.it, values_input)
+    in
+    Ok (ctx, value_output)
   in
-  let result = invoke_func' () in
   Instrumentation.Hooks.notify_func_exit ~id:id.it ~at:id.at;
   result
   |> nest id.at

spectec/lib/interp/eval_sl/interp.ml
@@ -1066,14 +1066,15 @@ and invoke_rel (ctx : Ctx.t) (id : id) (values_input : value list) :
     result
   in
   let result =
-    if Cache.is_cached_rule id.it then (
-      let cache_result = Cache.Cache.find !rule_cache (id.it, values_input) in
-      match cache_result with
-      | Some values_output -> Some (ctx, values_output)
-      | None ->
-          let* ctx, values_output = attempt_rules () in
-          Cache.Cache.add !rule_cache (id.it, values_input) values_output;
-          Some (ctx, values_output))
+    if Cache.is_cached_rule id.it then
+      let invoke () =
+        match attempt_rules () with
+        | Some (_, values_output) -> Ok values_output
+        | None -> Error ()
+      in
+      match invoke |> Cache.with_cache rule_cache (id.it, values_input) with
+      | Ok values_output -> Some (ctx, values_output)
+      | Error _ -> None
     else attempt_rules ()
   in
   Instrumentation.Hooks.notify_rel_exit ~id:id.it ~at:id.at
@@ -1084,9 +1085,9 @@ and invoke_rel (ctx : Ctx.t) (id : id) (values_input : value list) :
 
 and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
     Ctx.t * value =
+  let ctx, values_input = eval_args ctx args in
   (* Builtin function invocation *)
   let invoke_func_builtin () =
-    let ctx, values_input = eval_args ctx args in
     let value_output =
       Builtins.invoke id targs values_input |> unwrap_builtin
     in
@@ -1121,7 +1122,6 @@ and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
           Ctx.add_typdef Local ctx_local tparam ([], Il.PlainT targ $ targ.at))
         ctx_local tparams targs
     in
-    let ctx, values_input = eval_args ctx args in
     let attempt_clauses () =
       Instrumentation.Hooks.notify_clause_enter ~id:id.it ~clause_idx:0
         ~at:id.at;
@@ -1134,22 +1134,32 @@ and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
       | Ret value_output -> (ctx, value_output)
       | _ -> error id.at "function was not matched"
     in
-    if Cache.is_cached_func id.it then (
-      let cache_result = Cache.Cache.find !func_cache (id.it, values_input) in
-      match cache_result with
-      | Some value_output -> (ctx, value_output)
-      | None ->
-          let ctx, value_output = attempt_clauses () in
-          Cache.Cache.add !func_cache (id.it, values_input) value_output;
-          (ctx, value_output))
-    else attempt_clauses ()
+    attempt_clauses ()
   in
   (* Main dispatch *)
   Instrumentation.Hooks.notify_func_enter ~id:id.it ~at:id.at ~values:[];
-  let result =
-    if Builtins.is_builtin id then invoke_func_builtin ()
-    else invoke_func_def ()
+  let invoke_func' () =
+    let invoke () =
+      let _, v =
+        if Builtins.is_builtin id then invoke_func_builtin ()
+        else invoke_func_def ()
+      in
+      Ok v
+    in
+    let value_output_result =
+      if
+        (not (Cache.is_cached_func id.it))
+        || targs <> []
+        || List.exists
+             (fun value ->
+               match value.it with Lang.Il.FuncV _ -> true | _ -> false)
+             values_input
+      then invoke ()
+      else invoke |> Cache.with_cache func_cache (id.it, values_input)
+    in
+    (ctx, value_output_result |> Result.get_ok)
   in
+  let result = invoke_func' () in
   Instrumentation.Hooks.notify_func_exit ~id:id.it ~at:id.at;
   result
 

spectec/lib/interp/builtins/p4/fresh.ml
@@ -1,4 +1,5 @@
 module Il = Lang.Il
+module Cache = Semantics.Dynamic.Cache
 open Il
 open Common.Source
 

spectec/lib/semantics/dynamic/cache.ml
@@ -64,8 +64,6 @@ module Cache = struct
   let add cache key value = Table.add cache key value
 end
 
-(* Cache targets *)
-
 let is_cached_func = function
   | "subst_type" | "subst_typeDef" | "specialize_typeDef" | "canon"
   | "free_type" | "is_nominal_typeIR" | "bound" | "gen_constraint_type"
@@ -79,3 +77,12 @@ let is_cached_rule = function
   | "Sub_impl_canon" | "Sub_impl_canon_neq" | "Type_wf" | "Type_alpha" ->
       true
   | _ -> false
+
+let with_cache cache (id, values) compute =
+  let key = (id, values) in
+  match Cache.find !cache key with
+  | Some v -> Ok v
+  | None ->
+      let result = compute () in
+      (match result with Ok v -> Cache.add !cache key v | _ -> ());
+      result

spectec/test/il/pos.expected
@@ -3841,10 +3841,10 @@ Il typing summary
   Skipped:    215 (16.82% of total)
 
   Duration (s):
-    Total:     194.953
-    Mean:     0.183399
-    Median:   0.182013
-    Min:      0.022705
-    Max:      0.758296
-    P95:      0.645575
+    Total:     185.619
+    Mean:     0.174618
+    Median:   0.178568
+    Min:      0.023098
+    Max:      0.641489
+    P95:      0.570304
 

spectec/test/sl/neg.expected
@@ -1191,10 +1191,10 @@ Sl typing summary
   Skipped:     56 (18.06% of total)
 
   Duration (s):
-    Total:       7.958
-    Mean:     0.031330
-    Median:   0.023968
-    Min:      0.021088
-    Max:      0.145139
-    P95:      0.073212
+    Total:       7.929
+    Mean:     0.031215
+    Median:   0.023819
+    Min:      0.020952
+    Max:      0.142865
+    P95:      0.074126
 

spectec/test/sl/pos.expected
@@ -3841,10 +3841,10 @@ Sl typing summary
   Skipped:    215 (16.82% of total)
 
   Duration (s):
-    Total:     118.332
-    Mean:     0.111319
-    Median:   0.107657
-    Min:      0.021953
-    Max:      0.448233
-    P95:      0.379981
+    Total:     112.755
+    Mean:     0.106072
+    Median:   0.105689
+    Min:      0.022025
+    Max:      0.397601
+    P95:      0.336694
 

commit 7aae03b4adb12d45cf954fd36438ab38225b478f

fix(interp): fix `notify_rel/func_enter` not being called on cache hit
Trace instrumentation was failing because `notify_exit` was being
called more frequently than `notify_enter`. This moves `notify_enter`
outside of cached computation so that they are always called.

Fixes: 567ad861aadd ("refactor(cache): wrapper for cache usage, prevent HOF caching")

spectec/lib/interp/eval_il/interp.ml
@@ -849,6 +849,8 @@ and eval_iter_prem (ctx : Ctx.t) (prem : prem) (iterexp : iterexp) :
 
 and invoke_rel (ctx : Ctx.t) (id : id) (values_input : value list) :
     (Ctx.t * value list) attempt =
+  Instrumentation.Hooks.notify_rel_enter ~id:id.it ~at:id.at
+    ~values:values_input;
   (* Rule matching *)
   let match_rule ctx inputs rule values_input =
     let _, notexp, prems = rule.it in
@@ -864,8 +866,6 @@ and invoke_rel (ctx : Ctx.t) (id : id) (values_input : value list) :
   in
   (* Main invocation logic *)
   let invoke_rel' () =
-    Instrumentation.Hooks.notify_rel_enter ~id:id.it ~at:id.at
-      ~values:values_input;
     (* Find the relation *)
     let inputs, rules = Ctx.find_rel Local ctx id in
     check_warn (rules <> []) id.at "relation has no rules";
@@ -927,6 +927,8 @@ and invoke_rel (ctx : Ctx.t) (id : id) (values_input : value list) :
 and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
     (Ctx.t * value) attempt =
   let ctx, values_input = eval_args ctx args in
+  Instrumentation.Hooks.notify_func_enter ~id:id.it ~at:id.at
+    ~values:values_input;
   (* Clause matching *)
   let match_clause ctx_caller ctx_callee clause values_input =
     let args_input, exp_output, prems = clause.it in
@@ -938,8 +940,6 @@ and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
   in
   (* Builtin function invocation *)
   let invoke_func_builtin () =
-    Instrumentation.Hooks.notify_func_enter ~id:id.it ~at:id.at
-      ~values:values_input;
     (* Invoke builtin function *)
     let invoke_func_builtin' () =
       Instrumentation.Hooks.notify_clause_enter ~id:id.it ~clause_idx:0
@@ -975,8 +975,6 @@ and invoke_func (ctx : Ctx.t) (id : id) (targs : targ list) (args : arg list) :
           in
           List.map (Typ.subst_typ theta) targs
     in
-    Instrumentation.Hooks.notify_func_enter ~id:id.it ~at:id.at
-      ~values:values_input;
     (* Apply the first matching clause *)
     let attempt_clauses () =
       let attempt_clauses' =