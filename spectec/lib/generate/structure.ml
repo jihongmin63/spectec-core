bb82db4d57cb8c3ad3f3b4bb458d4febd3866210
Min <jihongmin63@gmail.com>
2026-04-09 13:42:21 +0900

    fix: mismatches in il semantics

--- spectec/lib/generate/structure.ml
+++ spectec/lib/generate/structure.ml
@@ -3,4 +3,4 @@ module Il = Lang.Il
 let find_structure spec relation binding_prems =
   let _ = Format.printf "%s\n" (Lang.Il.Print.string_of_def relation) in
   let _ = spec, relation, binding_prems in
-  ()
+  ()                                  
